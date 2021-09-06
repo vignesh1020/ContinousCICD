76f2aacf5fd4a7de6ad65f2de094ef155c9be4e9
unknown <vickyvignesh22092000@gmail.com>
2021-09-06 14:56:51 +0530

    deleted
    Signed-off-by: unknown <vickyvignesh22092000@gmail.com>

---
+++ java_eclipse/src/java_eclipse/sample_class.java
@@ -0,0 +1,10 @@
+package java_eclipse;
+
+public class sample_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sample class");
+	}
+
+}
