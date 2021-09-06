f3e3e70d3028eedfb09426dc282a5243b2d6d622
unknown <vickyvignesh22092000@gmail.com>
2021-09-06 15:01:50 +0530

    java
    Signed-off-by: unknown <vickyvignesh22092000@gmail.com>

--- java_eclipse/src/java_eclipse/sample_class.java
+++ java_eclipse/src/java_eclipse/sample_class.java
@@ -5,6 +5,7 @@ public class sample_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("sample class");
+System.out.println("sample 2");
 	}
 
 }
